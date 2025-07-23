6d925e804c0d2d9a53106ef65128cb7f97e80bc0
mwamba <mwambajoakim@gmail.com>
2025-07-23 20:25:15 +0300

    Make executable

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+?\w+)\]\s\[to:(\+?\w+)\]\s\[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/).join
+
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <log_file>"
+  exit 1
+end
+
+# Read file contents
+file = ARGV[0]
+
+# Regex pattern for [from:], [to:], [flags:]
+pattern = /\[from:(\+?\w+)\]\s\[to:(\+?\w+)\]\s\[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/
+
+# Scan each matching line and print in [SENDER],[RECEIVER],[FLAGS] format
+File.foreach(file) do |line|
+  if match = line.match(pattern)
+    puts "#{match[1]},#{match[2]},#{match[3]}"
+  end
+end
